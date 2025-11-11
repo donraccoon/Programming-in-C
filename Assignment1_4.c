be97ef8d40ead51c6ebd374ced461ec858f032ef
Donraccoon <larsmeijer20@gmail.com>
2025-11-11 19:14:28 +0100

    added comments to 1.4

--- Assignment1_4.c
+++ Assignment1_4.c
@@ -4,24 +4,21 @@ Number: s3662780
 Assignment: 1.4
 */
 
-#include<stdio.h>
-#include<math.h>
+#include<stdio.h> 
+#include<math.h> //for r^n
 
 int main(void){
-    double a; 
-    double r;
-    int N;
-    double sum = 0.0;
-    int n;
-    N = 0;
+    double a, r; //makes a and r doubles
+    int N, n; // N and n intergers
+    double sum = 0.0; // for easy output
     
-    scanf("%lf%d%lf",&a ,&N, &r);
-    printf("\na is %.2lf, N is %d , r is %.2lf", a, N, r);
+    scanf("%lf%d%lf",&a ,&N, &r); // user in puts
+    //printf("\na is %.2lf, N is %d , r is %.2lf", a, N, r); //prints user inputs for testing
 
-    for (n = 0; n <= N; n++){
-        sum += a*pow(r, n);
+    for (n = 0; n <= N; n++){ //a forloop to make the sum
+        sum += a*pow(r, n); //the base function
     }
 
-    printf("\n%0.2lf\n", sum);
+    printf("%0.2lf\n", sum);//prints the final value of the sum
 
 }
